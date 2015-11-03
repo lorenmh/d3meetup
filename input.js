9a023e3408d77c08a0eaefb46c99544e8fefd9ae
Sawa <nakamods320yen@gmail.com>
2015-11-02 20:36:30 -0800

    load world topojson

--- input.js
+++ input.js
@@ -1,5 +1,9 @@
 var d3 = require('d3');
+// var queue = require('queue');
 console.dir(d3);
 d3.csv( 'events.csv', function(d) {
   console.log( d );
+  d3.json("topo_world_countries_with_meta.json", function(err, world){
+    console.dir(world);
+  });
 })
