ea3fcda07eb2c67d7919bb04f7cb9431abf2d7f3
Sawa <nakamods320yen@gmail.com>
2015-11-02 20:45:05 -0800

    draw world map

--- input.js
+++ input.js
@@ -1,9 +1,16 @@
 var d3 = require('d3');
-// var queue = require('queue');
+var topojson = require('topojson');
 console.dir(d3);
 d3.csv( 'events.csv', function(d) {
   console.log( d );
   d3.json("topo_world_countries_with_meta.json", function(err, world){
     console.dir(world);
+    var svg = d3.select('#target').append('svg')
+      .attr('width', 960)
+      .attr('height', 800);
+    svg.append('path')
+      .datum(topojson.feature(world, world.objects.countries))
+      .attr("d", d3.geo.path().projection(d3.geo.mercator()));
+    ;
   });
 })
